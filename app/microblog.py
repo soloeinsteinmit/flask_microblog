b736d1f586d82536933c6c0189566aa37a4e3bed
soloeinsteinmit <88045720+soloeinsteinmit@users.noreply.github.com>
2024-08-29 16:23:50 +0100

    Working databases in Flask(Setup Flask Migration and SQLAlchemy

--- app/microblog.py
+++ app/microblog.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from routes import microblog
 from config import Config
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # register the blueprint with the app
 app.register_blueprint(microblog)
 app.config.from_object(Config)
+# Migration initialization
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
