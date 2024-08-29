b6f557b83678f8f9eae55987ac2e689a84e69415
soloeinsteinmit <88045720+soloeinsteinmit@users.noreply.github.com>
2024-08-29 07:58:46 +0100

    Working with forms

--- app/microblog.py
+++ app/microblog.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from routes import microblog
+from config import Config
 
 app = Flask(__name__)
 
 # register the blueprint with the app
 app.register_blueprint(microblog)
+app.config.from_object(Config)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, flash, redirect
+from forms import LoginForm
 
 microblog = Blueprint('main', __name__)
 
@@ -18,3 +19,10 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+@microblog.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index')
+    return render_template('login.html', title="Sign In", form=form)
