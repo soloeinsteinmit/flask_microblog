509ab7d11e564cb073074c0e6a11d9cb709f98ad
soloeinsteinmit <88045720+soloeinsteinmit@users.noreply.github.com>
2024-08-29 13:31:16 +0100

    Improving form validation and generating links with url_for function

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, flash, redirect
+from flask import render_template, Blueprint, flash, redirect, url_for
 from forms import LoginForm
 
 microblog = Blueprint('main', __name__)
@@ -24,5 +24,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect('/index')
+        return redirect(url_for('main.index'))
     return render_template('login.html', title="Sign In", form=form)
